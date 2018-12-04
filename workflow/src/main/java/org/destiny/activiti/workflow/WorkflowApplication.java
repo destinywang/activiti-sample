132e3b8eb782fb957afe346ff18678db89802a40
destiny <destinywk@163.com>
2018-12-04 15:21:43 +0800

    workflow

---
+++ workflow/src/main/java/org/destiny/activiti/workflow/WorkflowApplication.java
@@ -0,0 +1,12 @@
+package org.destiny.activiti.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowApplication.class, args);
+    }
+}
