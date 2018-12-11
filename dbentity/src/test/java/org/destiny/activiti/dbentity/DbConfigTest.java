6bcee4a47d66fc2f79254acdc8994b52d335870f
destiny <destinywk@163.com>
2018-12-11 22:56:00 +0800

    db

---
+++ dbentity/src/test/java/org/destiny/activiti/dbentity/DbConfigTest.java
@@ -0,0 +1,23 @@
+package org.destiny.activiti.dbentity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author destiny
+ * destinywk@163.com
+ * ------------------------------------------------------------------
+ * <p></p>
+ * ------------------------------------------------------------------
+ * design by 2018/12/11 22:47
+ * @version 1.8
+ * @since JDK 1.8.0_101
+ */
+@Slf4j
+public class DbConfigTest {
+
+    @Test
+    public void testDbConfig() {
+
+    }
+}
