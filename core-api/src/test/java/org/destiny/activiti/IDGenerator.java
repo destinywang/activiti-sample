ce7041aa6a356f9689fae9b6f23860ad42c1e37f
destiny <destinywk@163.com>
2018-12-19 00:08:21 +0800

    subTask

---
+++ core-api/src/test/java/org/destiny/activiti/IDGenerator.java
@@ -0,0 +1,17 @@
+package org.destiny.activiti;
+
+/**
+ * @author destiny
+ * destinywk@163.com
+ * ------------------------------------------------------------------
+ * <p></p>
+ * ------------------------------------------------------------------
+ * design by 2018/12/18 23:01
+ * @version 1.8
+ * @since JDK 1.8.0_101
+ */
+public class IDGenerator {
+    public static String generateID() {
+        return null;
+    }
+}
---
+++ core-api/src/test/java/org/destiny/activiti/PrimaryTest.java
@@ -0,0 +1,25 @@
+package org.destiny.activiti;
+
+import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.persistence.entity.TaskEntity;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author destiny
+ * destinywk@163.com
+ * ------------------------------------------------------------------
+ * <p></p>
+ * ------------------------------------------------------------------
+ * design by 2018/12/18 22:40
+ * @version 1.8
+ * @since JDK 1.8.0_101
+ */
+public class PrimaryTest {
+
+}
